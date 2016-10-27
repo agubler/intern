f22a3f04ccd2c09ff165a799fa49ea0468c65ab0
Nick Nisi <nick@nisi.org>
2016-10-26 19:09:11 -0500

    [ci skip] convert LcovHtml reporter

---
+++ src/lib/reporters/LcovHtml.ts
@@ -0,0 +1,24 @@
+import { Collector } from 'dojo/node!istanbul/lib/collector';
+import { HtmlReport } from 'dojo/node!istanbul/lib/report/html';
+import { Reporter, ReporterConfig } from '../../interfaces';
+
+export class LcovHtmlReporter implements Reporter {
+	private _collector: Collector;
+	private _reporter: HtmlReport;
+
+	constructor(config: ReporterConfig = {}) {
+		this._collector = new Collector();
+		this._reporter = new HtmlReport({
+			dir: config.directory,
+			watermarks: config.watermarks
+		});
+	}
+
+	coverage(sessionId: string, coverage: Object): void {
+		this._collector.add(coverage);
+	}
+
+	runEnd(): void {
+		this._reporter.writeReport(this._collector, true);
+	}
+}
